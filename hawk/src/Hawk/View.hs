a95e98e76e0f85e7e1f2e30bc3e6e52ac3fb91ec
ubuntu <ubuntu@ubuntu.(none)>
2010-01-03 08:53:57 -0500

    view.hs

---
+++ hawk/src/Hawk/View.hs
@@ -0,0 +1,15 @@
+module Hawk.View
+  ( 
+    module Hawk.View.EmptyView
+  , module Hawk.View.TextView
+  , module Hawk.View.TemplateView
+  , module Hawk.View.Template.HtmlHelper
+  , module Hawk.View.Template.DataType
+  ) where
+
+import Hawk.View.EmptyView
+import Hawk.View.TextView
+import Hawk.View.TemplateView hiding (XmlTree) -- is imported by HtmlHelper from HXT
+import Hawk.View.Template.HtmlHelper
+import Hawk.View.Template.DataType
+
